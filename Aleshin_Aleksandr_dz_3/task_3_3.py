de440bc5e01b17bbc9a64b1dd5c30451d7a8ce51
Aleksandr <titanoos67@gmail.com>
2022-03-25 11:36:47 +0300

    3_3
    убрал лишние строки

--- Aleshin_Aleksandr_dz_3/task_3_3.py
+++ Aleshin_Aleksandr_dz_3/task_3_3.py
@@ -3,10 +3,11 @@
 def thesaurus(*args):
     name = {}
     for ls in args:
-        name.setdefault(ls[0], []) # создаю ключи с пустым списком
-        name[ls[0]] += [ls]  # прибавляю значения по ключам
+        name[ls[0]] = name.setdefault(ls[0], []) + [ls]  # создаю ключи с пустым списком, в список добавляется значение
     sort_name= sorted(name.items())
     return f'{name} \n{dict(sort_name)} sort'
+
+
 print(thesaurus("Мария", "Иван", "Петр", "Илья","Марина", "Анна", "Инна"))
 
 
